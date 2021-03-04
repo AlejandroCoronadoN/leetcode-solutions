04e4a6dd26b27048c029a04dad3f218516cc21b8
Alejandro Coronado <alejandro.coronado.n@gmail.com>
2021-03-04 11:01:39 -0600

    twosum

---
+++ twosum.py
@@ -0,0 +1,15 @@
+class Solution:
+  """
+  Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+  You may assume that each input would have exactly one solution, and you may not use the same element twice.
+  You can return the answer in any order.
+  """  
+  def twoSum(self, nums: List[int], target: int) -> List[int]:
+    i=0
+    while i < len(nums) - 1:
+      for j in range(i+1, len(nums)):
+        if nums[j] +  nums[i] == target:
+          return [i,j]
+      i += 1
+
+        
